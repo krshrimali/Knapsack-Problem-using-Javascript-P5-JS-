24af23060ca4603e502e8279258f39f4a76e8f29
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2017-10-18 09:42:31 +0530

    Added files via upload

---
+++ sketch_3.js
@@ -0,0 +1,20 @@
+// knapsack [array]
+// profit
+// weight
+function setup(){
+	noCanvas();
+}
+
+for(var i = 0; i < 5; i++){
+	p[i] = int(random(0, 50));
+}
+
+for(var i = 0; i < 5; i++){
+	w[i] = int(random(0, 50));
+}
+
+// capacity
+M = int(random(0, 50));
+
+console.log(p);
+console.log(w);
